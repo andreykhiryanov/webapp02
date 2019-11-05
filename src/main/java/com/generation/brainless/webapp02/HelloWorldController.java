99947c0f01f3cb83805d12e249a67fc6c30fe9c9
Andrey Khiryanov <andreykhiryanov@gmail.com>
2019-11-05 10:30:55 +0300

    Minor edits again

--- src/main/java/com/generation/brainless/webapp02/HelloWorldController.java
+++ src/main/java/com/generation/brainless/webapp02/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.generation.brainless.webapp02;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,7 +12,7 @@ public class HelloWorldController {
 
     @RequestMapping("/nameForm")
     public String showForm() {
-        return "name-form";
+        return "helloworld-form";
     }
 
     @RequestMapping("/ageForm")
@@ -38,6 +39,28 @@ public class HelloWorldController {
         // Create the message
 
         String result = "HEY, " + theName + "!!!";
+
+        // Add message to the model
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
+
+        // Reading the request parameter from the HTML form
+
+
+
+        // Convert the data to UPPER case
+
+        name = name.toUpperCase();
+
+        // Create the message
+
+        String result = "Hey, my friend, " + name + "!!!";
 
         // Add message to the model
 
